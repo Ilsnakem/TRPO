0fdddcca459d8468f80e04f3f5fd473fe80acd06
Ilsnakem <nik.furman.1999@mail.ru>
2022-12-22 13:04:02 +0300

    LabC#

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+namespace Lab7
+{
+    class Program
+    {
+        static void Main()
+        {
+            Regex r = new Regex(@"((https?|ftp)\:\/\/)?([a-z0-9]{1})((\.[a-z0-9-])|([a-z0-9-]))*\.([a-z]{2,6})(\/?)");//создание выражения
+            string text = Console.ReadLine();
+            Match www = r.Match(text);//поиск в тексте по шаблону
+            while (www.Success)
+            {
+                Console.WriteLine(www);
+                www = www.NextMatch();//поиск следующего совпадения
+            }
+        }
+    }
+}
